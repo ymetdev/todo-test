fc918471418c6fe0992173ca89cc4923f3c78457
Nattakit Rattanarak <temgoodboi@gmail.com>
2024-07-30 02:44:45 +0700

    แก้ไขการจัดตำแหน่งปุ่ม Delete ในคอมโพเนนต์ TodoList

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 function TodoList() {
   const [task, setTask] = useState(() => {
@@ -7,6 +7,7 @@ function TodoList() {
     return savedTasks ? JSON.parse(savedTasks) : []
   })
   const [newTask, setNewTask] = useState("")
+  const textareaRef = useRef(null)
 
   useEffect(() => {
     // Save task to localStorage whenever task changes
@@ -22,6 +23,9 @@ function TodoList() {
     if (newTask !== "") {
       setTask([...task, newTask])
       setNewTask("")
+      if (textareaRef.current) {
+        textareaRef.current.style.height = "auto"
+      }
     }
   }
 
@@ -37,34 +41,38 @@ function TodoList() {
           <h1 className="font-medium text-text text-xl mb-5">
             Enter Your Task...
           </h1>
-          <input
+          <textarea
+            ref={textareaRef}
             onChange={typeHandler}
             value={newTask}
-            type="text"
-            maxLength={14}
             placeholder="Type here"
-            className="placeholder-primarycl input input-bordered w-full max-w-xs mb-5 bg-backgroundcl text-text"
-          />
+            className="placeholder-primarycl textarea textarea-bordered w-full max-w-xs mb-5 bg-backgroundcl text-text resize-none h-auto"
+            rows={1}
+            onInput={(e) => {
+              e.target.style.height = "auto"
+              e.target.style.height = `${e.target.scrollHeight}px`
+            }}></textarea>
           <button
             onClick={addTask}
-            className="btn bg-primarycl text-textcl font-bold">
+            className="hover:text-text btn bg-primarycl text-textcl font-bold">
             + Add Task
           </button>
         </div>
       </div>
 
-      <div className="w-72 font-bold">
+      <div className="w-90 font-bold">
         {task.map((task, index) => (
-          <p
+          <div
             key={index}
-            className="w-72 bg-primarycl text-textcl px-5 py-2 rounded-lg mb-4 flex items-center justify-between ">
-            {index + 1}. {task}
+            className="  bg-primarycl text-textcl px-5 py-2 rounded-lg mb-4 flex flex-row items-start justify-between break-words word-break">
+            <p className="mr-2">{index + 1}.</p>
+            <p className="w-52 break-words word-break mr-2">{task}</p>
             <button
               onClick={() => deleteTask(index)}
-              className="btn bg-backgroundcl text-text border-none">
+              className="btn bg-backgroundcl text-text border-none self-center">
               Delete
             </button>
-          </p>
+          </div>
         ))}
       </div>
     </div>
